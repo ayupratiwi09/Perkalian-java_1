dd8aaf1434641d5473535b8208ff524e7b3bdc3b
ayupratiwi09 <38656224+ayupratiwi09@users.noreply.github.com>
2018-05-26 10:22:34 +0700

    Add files via upload

---
+++ Perkalian.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author ACER
+ */
+public class Perkalian {
+    public static void main(String[] args) {
+        int x = 8, y;
+        for(int i=1; i<=10; i++){
+            y = i*x;
+            System.out.println(i +" x " + x + " = " + y);
+        }
+    }
+}
